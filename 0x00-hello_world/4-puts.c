e4e75ba681c85db16c0e6b52e89cd6609c1b39fe
Lungile Mawila <lungilemawila5@gmail.com>
2023-07-13 17:31:05 -0700

    second c programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - entry point
-*return - always zero (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
